41f3c13c70754740aa39419bf07359a3fb811774
PfeifferH <15hp19@queensu.ca>
2018-12-27 15:34:10 -0500

    Removed template files

--- App.js
+++ App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Platform, StatusBar, StyleSheet, View, Text } from 'react-native';
 import { AppLoading, Asset, Font, Icon } from 'expo';
-import AppNavigator from './navigation/AppNavigator';
+import { HomeScreen } from './screens/HomeScreen';
 
 export default class App extends React.Component {
   state = {
@@ -9,20 +9,11 @@ export default class App extends React.Component {
   };
 
   render() {
-    if (!this.state.isLoadingComplete && !this.props.skipLoadingScreen) {
-      return (
-        <View>
-          <Text>Hello World!</Text>
-        </View>
-      );
-    } else {
-      return (
-        <View style={styles.container}>
-          {Platform.OS === 'ios' && <StatusBar barStyle="default" />}
-          <AppNavigator />
-        </View>
-      );
-    }
+    return (
+      <View>
+        <HomeScreen />
+      </View>
+    );
   }
 
   _loadResourcesAsync = async () => {
@@ -34,9 +25,6 @@ export default class App extends React.Component {
       Font.loadAsync({
         // This is the font that we are using for our tab bar
         ...Icon.Ionicons.font,
-        // We include SpaceMono because we use it in HomeScreen.js. Feel free
-        // to remove this if you are not using it in your app
-        'space-mono': require('./assets/fonts/SpaceMono-Regular.ttf'),
       }),
     ]);
   };
