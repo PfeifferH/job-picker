215623ebd655f559c81b8e5ea88f1b2bb3d74a59
PfeifferH <15hp19@queensu.ca>
2018-12-30 15:53:54 -0500

    Built initial home screen

--- App.js
+++ App.js
@@ -1,8 +1,7 @@
 import React from 'react';
-import { Platform, StatusBar, StyleSheet, View, Text } from 'react-native';
+import { Platform, StatusBar, StyleSheet, View, ImageBackground } from 'react-native';
 import { Asset, Font, Icon } from 'expo';
 import { HomeScreen } from './screens/HomeScreen';
-import "./constants/Colors"
 import Colors from './constants/Colors';
 
 export default class App extends React.Component {
@@ -13,8 +12,10 @@ export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <StatusBar barStyle="light-content" backgroundColor={Colors.tabIconDefault} />
-        <HomeScreen />
+        <ImageBackground style={styles.image} source={require('./assets/images/job-picker-background-main.png')}>
+          <StatusBar barStyle="light-content" backgroundColor={Colors.statusBarColor} />
+          <HomeScreen />   
+        </ImageBackground> 
       </View>
     );
   }
@@ -47,7 +48,14 @@ export default class App extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#4F6D7A',
+    backgroundColor: '#0091ca',
     paddingTop: Platform.OS == "ios" ? 0 : StatusBar.currentHeight,
+    fontFamily: 'Roboto'
   },
+  image: {
+    flex: 1,
+    width: null,
+    height: null,
+    resizeMode: 'cover'
+  }
 });
--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,14 +1,35 @@
 import React, { Component } from 'react'
-import { View, Text, TextInput, StyleSheet } from 'react-native'
-
+import { View, Text, TextInput, Button, StyleSheet } from 'react-native'
+import Colors from '../constants/Colors'
 export class HomeScreen extends Component {
 
+  constructor(props) {
+    super(props)
+    this.state = {
+      searchString: '',
+      location: ''
+    }  
+  }
+  
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.text}>Find a job that's right for you</Text>
-        <TextInput style={styles.input}/>
-        <TextInput style={styles.input}/>
+        <View style={styles.header}>
+          <Text style={styles.title}>Find a job</Text>
+        </View>
+      
+        <TextInput style={styles.input} placeholder='Keyword(s)' value={this.state.searchString} onChangeText={(e) => this.setState({ searchString: e })} />
+     
+        <TextInput style={styles.input} placeholder='Location' value={this.state.location} onChangeText={(e) => this.setState({ location: e})} />
+
+        <View style={styles.button}>
+          <Button color={Colors.buttonColor} title="Filters"></Button>
+        </View>
+
+        <View style={styles.button}>
+          <Button color={Colors.buttonColor} title="Search"></Button>
+        </View>
+
       </View>
     )
   }
@@ -19,8 +40,17 @@ const styles = StyleSheet.create({
     justifyContent: 'space-between',
     alignItems: 'stretch'
   },
+  header: {
+    height: 60,
+    backgroundColor: '#fff',
+    flexDirection: 'column',
+    justifyContent: 'space-around',
+    marginBottom: 30,
+  },
+  title: {
+    fontSize: 25,    
+  },
   text: {
-    alignSelf: 'center',
     fontSize: 20,
     color: '#fff'
   },
@@ -28,6 +58,11 @@ const styles = StyleSheet.create({
     margin: 20,
     height: 40,
     backgroundColor: '#fff',
-    textAlign: 'center'
+    textAlign: 'center',
+    borderRadius: 10
+  },
+  button: {
+    borderRadius: 10,
+    margin: 20
   }
 })
