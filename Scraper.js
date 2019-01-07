8dc05f8c9e01e4fe53d222f10a97e3aa6f07e876
PfeifferH <15hp19@queensu.ca>
2019-01-07 17:08:44 -0500

    Parse returned data as json

--- Scraper.js
+++ Scraper.js
@@ -4,18 +4,22 @@ export function getResults(params) {
 
   //Build url
   let url = 'https://www.linkedin.com/jobs/search/?keywords=' + params.searchString + '&location=' + params.locations[0]
-
+  let data = {}
   fetch(url).then((resp)=>{ return resp.text() }).then((text)=>{ 
-    // console.log(text)
     const $ = cheerio.load(text)
-    console.log(html)
     console.log('fired')
-    // $('script').each(function(i, element){
-    //   const a = $(this).prev();
-    //   // const metadata = {
-    //   //   url: a.child().child().attr('content')
-    //   // }
-    //   console.log(a.html());
-    // });
+    $('script').each(function(i, element){
+      const a = $(this).prev();
+      // const metadata = {
+      //   url: a.child().child().attr('content')
+      // }
+      let jsonData = String(a.html())
+      if(jsonData.indexOf('elements') !== -1) {
+        jsonData = jsonData.slice(4, jsonData.length - 3)
+        jsonData = JSON.parse(jsonData)
+        data = jsonData
+      } 
+    });
+    console.log(data)
   })
 }
