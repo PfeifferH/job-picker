4c9823ee71e1fc5beb6b3823972d004e53adcf96
PfeifferH <15hp19@queensu.ca>
2019-01-07 13:24:01 -0500

    Fetch linkedin search page content

--- Scraper.js
+++ Scraper.js
@@ -3,7 +3,19 @@ const cheerio = require('react-native-cheerio')
 export function getResults(params) {
 
   //Build url
-  let url = 'https://en.wikipedia.org/wiki/List_of_Presidents_of_the_United_States';
+  let url = 'https://www.linkedin.com/jobs/search/?keywords=' + params.searchString + '&location=' + params.locations[0]
 
-  fetch(url).then((resp)=>{ return resp.text() }).then((text)=>{ console.log(text) })
+  fetch(url).then((resp)=>{ return resp.text() }).then((text)=>{ 
+    // console.log(text)
+    const $ = cheerio.load(text)
+    console.log(html)
+    console.log('fired')
+    // $('script').each(function(i, element){
+    //   const a = $(this).prev();
+    //   // const metadata = {
+    //   //   url: a.child().child().attr('content')
+    //   // }
+    //   console.log(a.html());
+    // });
+  })
 }
